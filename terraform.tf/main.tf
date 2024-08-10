8bd4b12232a0ceb084900d70218fd6e062025dbf
sunnymisal <sunnymisal21@gmail.com>
2024-08-10 20:36:04 +0530

    commit

--- terraform.tf/main.tf
+++ terraform.tf/main.tf
@@ -23,9 +23,11 @@ resource "aws_instance" "this" {
 }
 
 
-resource "aws_instance" "this" {
+resource "aws_instance" "2nd" {
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
 
   tags = var.tags
+
+}
